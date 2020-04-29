da68f83534824e6a50ab80ac43c378ed10a3ec38
1653005 <1653005@student.hcmus.edu.vn>
2020-04-29 18:36:41 +0700

    Updates

---
+++ precache-manifest.7642b5e9e4fc717712c6fc0020915616.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2725b05c5f0f37237d30cf64c29bb9f8",
+    "url": "/CovidMap/index.html"
+  },
+  {
+    "revision": "271569450d93dc18c00e",
+    "url": "/CovidMap/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "65ae903d3751cfcc663f",
+    "url": "/CovidMap/static/css/main.d21d54fa.chunk.css"
+  },
+  {
+    "revision": "271569450d93dc18c00e",
+    "url": "/CovidMap/static/js/2.03dd2174.chunk.js"
+  },
+  {
+    "revision": "82f37f318fda4a2b093d3c4085204c3d",
+    "url": "/CovidMap/static/js/2.03dd2174.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65ae903d3751cfcc663f",
+    "url": "/CovidMap/static/js/main.f3652382.chunk.js"
+  },
+  {
+    "revision": "65581f8d17708eacd7c2",
+    "url": "/CovidMap/static/js/runtime-main.191382ad.js"
+  }
+]);
